55a3f6248a2734e2720cee5b86b48951a7903b8a
js <jonathanstein@live.com>
2019-04-17 13:16:44 -0700

    move contacts to top

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 import "animate.css/animate.min.css";
 import { ArrowDownCircle } from 'react-feather';
 import ScrollAnimation from 'react-animate-on-scroll';
-import PageProgress from 'react-page-progress';
-import  AnimatedText  from './AnimatedText';
+import AnimatedText from './AnimatedText';
 import StarsBackground from './StarsBackground';
 import Ground from './Ground';
 import LoaderSplash from './Loader';
+import { GitHub, Mail } from 'react-feather';
 
 class App extends PureComponent {
 
   componentDidMount() {
     const elmnt = document.getElementById("app");
-    elmnt.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+    elmnt.scrollIntoView({ block: 'start', behavior: 'smooth' });
   }
 
   scrollToProject = () => {
     const elmnt = document.getElementById("projects");
-    elmnt.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+    elmnt.scrollIntoView({ block: 'start', behavior: 'smooth' });
   }
 
   render() {
@@ -26,25 +26,36 @@ class App extends PureComponent {
       <div>
         <LoaderSplash />
         <div className="App" id="app">
-          <StarsBackground/>
+          <StarsBackground />
+          <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" initiallyVisible={true}>
+            <span className="contact">
+              <span className="github">
+                <a href="https://github.com/JStein92" target="_blank">
+                  <GitHub />
+                </a>
+              </span>
+              <a href="mailto:jonathanstein@live.com" className="email"><Mail /></a>
+            </span>
+          </ScrollAnimation>
+
           <div className='introText'>
-            <span className="nameTitle"><AnimatedText text="Jonathan Stein" fontSize="2" delay={3000} id="a"/></span>
+            <span className="nameTitle"><AnimatedText text="Jonathan Stein" fontSize="2" delay={3000} id="a" /></span>
             <br />
-            <span className="subtitle"><AnimatedText text="Web developer" fontSize="1.3" delay={5000} id="b"/></span>
-              <div className="seeMore">
-                <div>
-                  <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" initiallyVisible={false}>
+            <span className="subtitle"><AnimatedText text="Web developer" fontSize="1.3" delay={5000} id="b" /></span>
+            <div className="seeMore">
+              <div>
+                <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" initiallyVisible={false}>
                   Explore Below
                   <p className="arrow">
                     <ArrowDownCircle />
                   </p>
                 </ScrollAnimation>
-                </div>
               </div>
+            </div>
           </div>
-        <Ground />
+          <Ground />
+        </div>
       </div>
-    </div>
     );
   }
 
--- src/Ground.js
+++ src/Ground.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import anime from 'animejs'
-import { GitHub, Mail } from 'react-feather';
 import Projects from './Projects/Projects';
 import Skills from './Skills/Skills';
 
@@ -20,19 +19,19 @@ export default class Ground extends PureComponent {
   handleHover = () => {
     anime({
       targets: '.el',
-      translateY: [{value: -100, duration: 1000}, {value: 0, duration: 1000}],
+      translateY: [{ value: -100, duration: 1000 }, { value: 0, duration: 1000 }],
       delay(target, index) {
-          return (index* 30);
+        return (index * 30);
       },
       loop: true,
       easing: 'easeInOutQuad',
     });
   }
 
-  render () {
+  render() {
     const { props } = this;
     const blocks = [];
-    for (let i = 0; i < 25; i+=1) {
+    for (let i = 0; i < 25; i += 1) {
       blocks.push(i);
     }
     return (
@@ -40,28 +39,14 @@ export default class Ground extends PureComponent {
         <div id="waves">
           <div className="line">
             {
-              blocks.map((block, i ) => <div key={i} className="el" />)
+              blocks.map((block, i) => <div key={i} className="el" />)
             }
           </div>
         </div>
         <div className="waterBackground">
           <Projects projectRef={props.projectRef} />
           <Skills />
-            <span className="contact contactMobile">
-              <span className="github">
-                <a href="https://github.com/JStein92" target="_blank">
-                  <GitHub />
-                </a>
-              </span>
-              <a href="mailto:jonathanstein@live.com" className="email"><Mail /></a>
-            </span>
-            <span className="contact contactDesktop">
-              <span className="phone">425.214.6105</span>
-              <span className="github"><a href="https://github.com/JStein92" target="_blank">
-                <GitHub />
-              </a></span>
-            <a href="mailto:jonathanstein@live.com" className="email">jonathanstein@live.com</a>
-            </span>
+
         </div>
       </div>
 
